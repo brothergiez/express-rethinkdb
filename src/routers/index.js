3f20c31ac4e43bcfa5843d3266135d0258f6d8a4
Cristoph Adam Sugianto <giezbungadeza@gmail.com>
2020-05-17 02:09:19 +0700

    refactoring routes

--- src/routers/index.js
+++ src/routers/index.js
@@ -2,6 +2,4 @@ const users = require('./users');
 
 const routers = [...users];
 
-module.exports = (app) => {
-  routers.forEach(router => app.use(routers));
-}
+module.exports = routers;
--- src/server/index.js
+++ src/server/index.js
@@ -11,5 +11,5 @@ const db = setupMiddleware(app, config, thinky);
 app.use(db);
 app.use(express.json());
 app.use(express.urlencoded({extended: true}));
-routes(app);
+app.use(routes);
 app.listen(config.httpPort, () => console.log(`server run at http://localhost:${config.httpPort}`));
